efbcd7ef707cbe55a9b6320f28fd44a33dd46c14
Your Name <you@example.com>
2026-07-31 12:48:08 +1200

    Added feature list to pricing_page.html.

---
+++ billing/metadata.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from . import features
+
+
+@dataclass
+class ProductMetadata:
+    """
+    Metadata for a Stripe product.
+    """
+    stripe_id: str
+    name: str
+    features: list[str]
+    description: str = ""
+    is_default: bool = False
+
+
+PAPERTRAIL_PRO = ProductMetadata(
+    stripe_id="prod_UynmT2hCmUF08u",
+    name="Papertrail Pro",
+    description="For small businesses and teams",
+    is_default=True,
+    features=[
+        features.UNLIMITED_SCANS,
+        features.BANK_TRANSACTION_SYNC,
+        features.QUICK_REIMBURSEMENT_REQUEST
+    ],
+)
+
+PRODUCTS = {
+    PAPERTRAIL_PRO.stripe_id: PAPERTRAIL_PRO,
+}
--- billing/views.py
+++ billing/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from djstripe.models import Product
-
+from . import metadata
 
 def pricing_page(request):
-    return render(request, 'billing/pricing_page.html', {
-        'products': Product.objects.all()
-    })
+    products = Product.objects.filter(active=True).prefetch_related("prices")
+
+    for product in products:
+        meta = metadata.PRODUCTS.get(product.id)
+        product.features_list = meta.features if meta else []
+
+    return render(
+        request, "billing/pricing_page.html", {"products": products}
+    )
