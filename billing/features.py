552ba0b7583c01b64011ac642a2d308cde4c1372
Your Name <you@example.com>
2026-07-31 12:10:05 +1200

    Updated features.py.

--- billing/features.py
+++ billing/features.py
@@ -1,3 +1,7 @@
-UNLIMITED_SCANS = "Unlimited Scans"
+UNLIMITED_SCANS = "Unlimited Quick Scans"
 BANK_TRANSACTION_SYNC = "Bank Transaction Sync"
-QUICK_REIMBURSEMENT_REQUEST = "Quick Reimbursement Request"
+QUICK_REIMBURSEMENT_REQUEST = "Quick Reimbursement Requests"
+
+LIMITED_SCANS = "Limited Scans"
+SUPPORTING_FILE_UPLOAD = "Supporting File Upload"
+EXPIRY_REMINDERS = "Record Expiry Reminders"
