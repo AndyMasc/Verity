8e6ce155f0daabd6f9571c5e5437eed9123307b3
Your Name <you@example.com>
2026-09-14 19:07:01 +1200

    feat: update form-action in CONTENT_SECURITY_POLICY to allow specific external URL

--- Verity/settings/base.py
+++ Verity/settings/base.py
@@ -176,7 +176,7 @@ CONTENT_SECURITY_POLICY = {
         ),
         "frame-ancestors": ("'none'",),
         "base-uri": ("'self'",),
-        "form-action": ("'self'",),
+        "form-action": ("'self'", "https://veritypay.app"),
         "object-src": ("'none'",),
     }
 }
